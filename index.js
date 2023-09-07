54cc713784bf8393909f92678bc504964624606b
Gregory Pappas <gnpappas@proton.me>
2023-09-06 17:45:45 -1000

    Code stuff

--- index.js
+++ index.js
@@ -1,3 +1,69 @@
+var lines = [
+    'It\'s me.',
+    "Can you hear me?",
+    "from the other side",
+    "from the outside",
+];
+for (let i = 0; i < lines.length; i++) {
+    lines[i] = "Hello, " + lines[i];
+}
+
+console.log(lines);
+/* var education = "a Master's degree";
+
+// set the value of this based on a person's education
+var salary = 0;
+
+switch (education) {
+    case "no high school diploma":
+        salary = 25636;
+        break;
+    case "a high school diploma":
+        salary = 35256;
+        break;
+    case "an Associate's degree":
+        salary = 41496;
+        break;
+    case "a Bachelor's degree":
+        salary = 59124;
+        break;
+    case "a Master's degree":
+        salary = 69732;
+        break;
+    case "a Professional degree":
+        salary = 89960;
+        break;
+    case "a Doctoral degree":
+        salary = 84396;
+}
+console.log(
+    `In 2015, a person with ${education} earned an average of $${salary.toLocaleString(
+        "en-US"
+    )}/year.`
+); */
+/* var eatsPlants = false;
+var eatsAnimals = true;
+
+var category =
+    eatsPlants && !eatsAnimals
+        ? "herbivore"
+        : eatsPlants && eatsAnimals
+        ? "omnivore"
+        : !eatsPlants && eatsAnimals
+        ? "carnivore"
+        : undefined;
+
+console.log(category); */
+
+/* function isLastEvenOdd(numArray) {
+    if (numArray[numArray.length - 1] % 2 === 0) {
+        return 'even';
+    } else {
+        return 'odd';
+    }
+}
+console.log(isLastEvenOdd([1,2,3,4,5])); */
+
 /* function toASCII(string) {
     for (let i = 0; i < string.length; i++) {
         console.log(string.charCodeAt(i));
